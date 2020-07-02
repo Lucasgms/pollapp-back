da2bf02ec7aeb52a6db9f6c5ff9f4c4b4f73d3c1
Lucas Gregory <lgregory12@hotmail.com>
2020-07-02 14:43:09 -0300

    feat: polls.
    add polls routes to app router.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import pollsRouter from '@modules/polls/infra/http/routes/polls.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/polls', pollsRouter);
 
 
 export default routes;
