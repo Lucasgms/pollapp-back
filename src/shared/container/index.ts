460f3a159bb9ce3160e5c4946534d9934f675b3c
Lucas Gregory <lgregory12@hotmail.com>
2020-07-02 11:47:26 -0300

    feat: polls.
    handle Polls repository dependency injection.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,7 +4,15 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IPollsRepository from '@modules/polls/repositories/IPollsRepository';
+import PollsRepository from '@modules/polls/infra/typeorm/repositories/PollsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IPollsRepository>(
+  'PollsRepository',
+  PollsRepository,
 );
