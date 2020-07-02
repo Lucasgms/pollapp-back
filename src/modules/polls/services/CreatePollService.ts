d3126a2072ece6ba98167b8e7363290515be1016
Lucas Gregory <lgregory12@hotmail.com>
2020-07-02 11:47:40 -0300

    feat: polls.
    add create poll service.

---
+++ src/modules/polls/services/CreatePollService.ts
@@ -0,0 +1,33 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Poll from '../infra/typeorm/entities/Poll';
+import IPollsRepository from '../repositories/IPollsRepository';
+import ICreatePollDTO from '../dtos/ICreatePollDTO';
+
+@injectable()
+class createPollService {
+  constructor(
+    @inject('PollsRepository')
+    private pollsRepository: IPollsRepository
+  ) {}
+
+  public async execute({
+    title,
+    description,
+    options,
+    is_public,
+    owner_id,
+  }: ICreatePollDTO): Promise<Poll> {
+    const poll = await this.pollsRepository.create({
+      title,
+      description,
+      options,
+      is_public,
+      owner_id,
+    });
+
+    return poll;
+  }
+}
