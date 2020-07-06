db35442c447e3671180da28dd24e8b622b23d7cf
Lucas Gregory <lgregory12@hotmail.com>
2020-07-06 10:06:43 -0300

    feat: list polls.
    add index method to polls controller.

--- src/modules/polls/infra/http/controllers/PollsController.ts
+++ src/modules/polls/infra/http/controllers/PollsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreatePollService from '@modules/polls/services/CreatePollService';
+import ListUserPollsService from '@modules/polls/services/ListUserPollsService';
 
 export default class PollsController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -24,5 +25,16 @@ export default class PollsController {
     });
 
     return response.json(poll);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listPolls = container.resolve(ListUserPollsService);
+    const { id: owner_id  } = request.user;
+
+    const polls = await listPolls.execute({
+      owner_id,
+    });
+
+    return response.json(polls);
   }
 }
