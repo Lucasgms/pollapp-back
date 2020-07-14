457489d88d22bcfebcbef967830ad20b13d6cd47
Lucas Gregory <lgregory12@hotmail.com>
2020-07-14 11:23:28 -0300

    feat: answers.
    add create answers validations.

--- src/modules/answers/services/CreateAnswerService.ts
+++ src/modules/answers/services/CreateAnswerService.ts
@@ -2,15 +2,50 @@ import { injectable, inject } from 'tsyringe';
 import IAnswersRepository from '../repositories/IAnswersRepository';
 import ICreateAnswerDTO from '../dtos/ICreateAnswerDTO';
 import Answer from '../infra/typeorm/entities/Answer';
+import IPollsRepository from '@modules/polls/repositories/IPollsRepository';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IPollOptionsDTO from '@modules/polls/dtos/IPollOptionsDTO';
+import IPollOptionDTO from '@modules/polls/dtos/IPollOptionDTO';
+
 
 @injectable()
 class CreateAnswerService {
   constructor(
     @inject('AnswersRepository')
     private answersRepository: IAnswersRepository,
+
+    @inject('PollsRepository')
+    private pollsRepository: IPollsRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
   ) {}
 
   public async execute({ poll_id, user_id, option }: ICreateAnswerDTO): Promise<Answer> {
+    if (user_id) {
+      const foundUser = await this.usersRepository.findById(user_id);
+
+      if (!foundUser) {
+        throw new AppError('User does not exist', 401);
+      }
+    }
+
+    const foundPoll = await this.pollsRepository.findById(poll_id);
+
+    if (!foundPoll) {
+      throw new AppError('Poll does not exist', 401);
+    }
+
+    const pollOptions = foundPoll.options as Object;
+    const options = pollOptions as IPollOptionsDTO;
+
+    const foundOption = options.options.find((optionItem: IPollOptionDTO) => optionItem.id === option);
+
+    if (!foundOption) {
+      throw new AppError('Option does not exist in this poll', 401);
+    }
+
     const answer = await this.answersRepository.create({
       poll_id,
       user_id,
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,12 +3,13 @@ import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import pollsRouter from '@modules/polls/infra/http/routes/polls.routes';
+import answersRouter from '@modules/answers/infra/http/routes/answers.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/polls', pollsRouter);
-
+routes.use('/answers', answersRouter);
 
 export default routes;
